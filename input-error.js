8fa2e06e9b6505ed50c9ddf8adef559e11b5c47b
Debabrata Saha <sahadebabrata570@gmail.com>
2021-08-24 22:31:47 +0600

    input error

---
+++ input-error.js
@@ -0,0 +1,20 @@
+function add(num1, num2) {
+    const sum = num1 + num2
+    return sum;
+}
+
+const firstTotal = add(63, 67)
+console.log(firstTotal)
+
+const secondTotal = add(54, 981)
+console.log(secondTotal)
+
+
+function Multiply(num1, num2) {
+    const result = num1 * num2;
+    return result;
+}
+
+const firstResult = Multiply(13 * 12); //common error; 
+// const firstResult = Multiply(13 + 12); //right way; 
+console.log(firstResult)
